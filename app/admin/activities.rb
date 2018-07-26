2bce5dfaa4cbe9bd1be39d0460de38029b123cb6
Ricky <rickycxj@gmail.com>
2018-07-26 17:26:28 +0800

    增加活动管理后台

---
+++ app/admin/activities.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Activity do
+  menu priority: 12
+  actions :all, except: [:new, :create, :edit, :update, :destroy]
+
+  filter :name
+  filter :departure_city
+  filter :destination
+  filter :created_at, as: :date_range
+
+  index do
+    render 'index', context: self
+  end
+end
